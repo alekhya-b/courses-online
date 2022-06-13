1da1ff2e6ac44c8e4c28ff07a6ced40c5fa16398
alekhya-b <bolsekaralekhya@gmail.com>
2022-06-13 10:16:45 +0530

    Updates

---
+++ precache-manifest.e5e36655a2d3631f1282c35dc400321c.js
@@ -0,0 +1,94 @@
+self.__precacheManifest = [
+  {
+    "revision": "743b85d2770e5def1301",
+    "url": "/courses-online/static/css/main.96889a08.chunk.css"
+  },
+  {
+    "revision": "743b85d2770e5def1301",
+    "url": "/courses-online/static/js/main.75298d0b.chunk.js"
+  },
+  {
+    "revision": "947f5d2982b9d1271fee",
+    "url": "/courses-online/static/js/runtime~main.5eb6fa21.js"
+  },
+  {
+    "revision": "6a05bb00ce45f5757883",
+    "url": "/courses-online/static/css/2.25f74688.chunk.css"
+  },
+  {
+    "revision": "6a05bb00ce45f5757883",
+    "url": "/courses-online/static/js/2.b286f0b0.chunk.js"
+  },
+  {
+    "revision": "152080b6073f97716861",
+    "url": "/courses-online/static/js/3.af919cd0.chunk.js"
+  },
+  {
+    "revision": "9c74e172f87984c48ddf5c8108cabe67",
+    "url": "/courses-online/static/media/flags.9c74e172.png"
+  },
+  {
+    "revision": "0ab54153eeeca0ce03978cc463b257f7",
+    "url": "/courses-online/static/media/icons.0ab54153.woff2"
+  },
+  {
+    "revision": "8e3c7f5520f5ae906c6cf6d7f3ddcd19",
+    "url": "/courses-online/static/media/icons.8e3c7f55.eot"
+  },
+  {
+    "revision": "faff92145777a3cbaf8e7367b4807987",
+    "url": "/courses-online/static/media/icons.faff9214.woff"
+  },
+  {
+    "revision": "b87b9ba532ace76ae9f6edfe9f72ded2",
+    "url": "/courses-online/static/media/icons.b87b9ba5.ttf"
+  },
+  {
+    "revision": "cd6c777f1945164224dee082abaea03a",
+    "url": "/courses-online/static/media/outline-icons.cd6c777f.woff2"
+  },
+  {
+    "revision": "701ae6abd4719e9c2ada3535a497b341",
+    "url": "/courses-online/static/media/outline-icons.701ae6ab.eot"
+  },
+  {
+    "revision": "82f60bd0b94a1ed68b1e6e309ce2e8c3",
+    "url": "/courses-online/static/media/outline-icons.82f60bd0.svg"
+  },
+  {
+    "revision": "962a1bf31c081691065fe333d9fa8105",
+    "url": "/courses-online/static/media/icons.962a1bf3.svg"
+  },
+  {
+    "revision": "ef60a4f6c25ef7f39f2d25a748dbecfe",
+    "url": "/courses-online/static/media/outline-icons.ef60a4f6.woff"
+  },
+  {
+    "revision": "ad97afd3337e8cda302d10ff5a4026b8",
+    "url": "/courses-online/static/media/outline-icons.ad97afd3.ttf"
+  },
+  {
+    "revision": "13db00b7a34fee4d819ab7f9838cc428",
+    "url": "/courses-online/static/media/brand-icons.13db00b7.eot"
+  },
+  {
+    "revision": "a046592bac8f2fd96e994733faf3858c",
+    "url": "/courses-online/static/media/brand-icons.a046592b.woff"
+  },
+  {
+    "revision": "c5ebe0b32dc1b5cc449a76c4204d13bb",
+    "url": "/courses-online/static/media/brand-icons.c5ebe0b3.ttf"
+  },
+  {
+    "revision": "e8c322de9658cbeb8a774b6624167c2c",
+    "url": "/courses-online/static/media/brand-icons.e8c322de.woff2"
+  },
+  {
+    "revision": "a1a749e89f578a49306ec2b055c073da",
+    "url": "/courses-online/static/media/brand-icons.a1a749e8.svg"
+  },
+  {
+    "revision": "4d3d620e9ec0fb8b33f355cbf9113104",
+    "url": "/courses-online/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/courses-online/precache-manifest.e5e36655a2d3631f1282c35dc400321c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/courses-online/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
